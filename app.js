2f6494449f02877d10a3a8aa7fbc6d1255708800
Vijay-Nataraj <victoryvijayn@gmail.com>
2025-03-23 19:16:33 +0530

    cors updated

--- app.js
+++ app.js
@@ -10,12 +10,26 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser());
 app.use(morgan("dev"));
-app.use(
-  cors({
-    origin: "https://vjn-freelance-marketplace.netlify.app",
-    credentials: true,
-  })
-);
+
+// Configure CORS to allow requests from 'http://localhost:5173' and 'https://vjn-freelance-marketplace.netlify.app'
+const allowedOrigins = [
+  "https://vjn-freelance-marketplace.netlify.app",
+  "http://localhost:5173",
+];
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
+      // Allow if origin is in allowed list or if it's a same-origin request
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  credentials: true,
+};
+
+// Apply cors to express.
+app.use(cors(corsOptions));
 
 app.use("/api/v1/user", authRouter);
 app.use("/api/v1/roles", roleRouter);
